5e5033ae66234199cce85b421c10fa27393aadab
nlok5923 <nlokhande5923@gmail.com>
2023-06-28 21:10:28 +0530

    added banana tutorial for next

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,126 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+// import styles from "../styles/Home.module.css";
+import { Banana, Chains } from "@rize-labs/banana-wallet-sdk";
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { SampleAbi } from "../abi/SampleAbi";
 
 export default function Home() {
+  useEffect(() => {
+    getBananaInstance();
+  }, []);
+
+  const [walletAddress, setWalletAddress] = useState("");
+  const [bananaSdkInstance, setBananSdkInstance] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [walletInstance, setWalletInstance] = useState(null);
+  const [output, setOutput] = useState("Welcome to Banana Demo");
+
+  const SampleContractAddress = "0x3EEe2FbB0e698531fca47f6d6A9cecF48aDBcB0c";
+
+  const getBananaInstance = () => {
+    const bananaInstance = new Banana(Chains.mumbai);
+    setBananSdkInstance(bananaInstance);
+  };
+
+  const createWallet = async () => {
+    setIsLoading(true);
+    const wallet = await bananaSdkInstance.createWallet();
+    setWalletInstance(wallet);
+    const address = await wallet.getAddress();
+    setWalletAddress(address);
+    setOutput("Wallet Address: " + address);
+    setIsLoading(false);
+  };
+
+  const connectWallet = async () => {
+    const walletName = bananaSdkInstance.getWalletName();
+    if (walletName) {
+      setIsLoading(true);
+      const wallet = await bananaSdkInstance.connectWallet(walletName);
+      setWalletInstance(wallet);
+      const address = await wallet.getAddress();
+      setWalletAddress(address);
+      setOutput("Wallet Address: " + address);
+      setIsLoading(false);
+    } else {
+      setIsLoading(false);
+      alert("You don't have wallet created!");
+    }
+  };
+
+  const makeTransaction = async () => {
+    setIsLoading(true);
+    const signer = walletInstance.getSigner();
+    const amount = "0.00001";
+    const tx = {
+      gasLimit: "0x55555",
+      to: SampleContractAddress,
+      value: ethers.utils.parseEther(amount),
+      data: new ethers.utils.Interface(SampleAbi).encodeFunctionData(
+        "stake",
+        []
+      ),
+    };
+
+    try {
+      const txn = signer.sendTransaction(tx);
+      setOutput(JSON.stringify(txn));
+    } catch (err) {
+      console.log(err);
+    }
+    setIsLoading(false);
+  };
+
+  const signMessage = async () => {
+    setIsLoading(true);
+    const sampleMsg = "Hello World";
+    const signer = walletInstance.getSigner();
+    const signMessageResponse = await signer.signBananaMessage(sampleMsg);
+    setOutput(JSON.stringify(signMessageResponse));
+    setIsLoading(false);
+  };
+
+  const getChainId = async () => {
+    setIsLoading(true);
+    const signer = walletInstance.getSigner();
+    const chainid = await signer.getChainId();
+    setOutput(JSON.stringify(chainid));
+    setIsLoading(false);
+  };
+
+  const getNetwork = async () => {
+    setIsLoading(true);
+    const provider = walletInstance.getProvider();
+    const network = await provider.getNetwork();
+    setOutput(JSON.stringify(network));
+    setIsLoading(false);
+  };
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+    <div className="App">
+      <h1>Banana SDK Demo</h1>
+      {walletAddress && <p> Wallet Address: {walletAddress}</p>}
+      <button className="btn" onClick={() => createWallet()}>
+        Create Wallet
+      </button>
+      <button className="btn" onClick={() => connectWallet()}>
+        Connect Wallet
+      </button>
+      <button className="btn" onClick={() => getChainId()}>
+        ChainId
+      </button>
+      <button className="btn" onClick={() => getNetwork()}>
+        Network
+      </button>
+      <button className="btn" onClick={() => makeTransaction()}>
+        Make transaction
+      </button>
+      <button className="btn" onClick={() => signMessage()}>
+        Sign message
+      </button>
+      <h1> Output Panel</h1>
+      <div className="output-div">
+        <p>{isLoading ? "Loading.." : output}</p>
+      </div>
     </div>
-  )
+  );
 }
